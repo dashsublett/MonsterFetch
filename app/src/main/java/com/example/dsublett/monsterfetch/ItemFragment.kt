b8353c3fa23199e1eb0d3d78cefc13df64d03bd6
Dashiell Sublett <dsublett@inmotionsoftware.com>
2019-02-01 17:09:42 -0600

    moved click listener logic to anonymous function in ItemFragment.onCreateView

--- app/src/main/java/com/example/dsublett/monsterfetch/ItemFragment.kt
+++ app/src/main/java/com/example/dsublett/monsterfetch/ItemFragment.kt
@@ -8,52 +8,36 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import com.squareup.moshi.Moshi
 import kotlinx.android.synthetic.main.monster_item_list.view.*
 import java.net.URL
 
-class ItemFragment : Fragment(), View.OnClickListener {
+class ItemFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         /* Inflate RecyclerView "monster_item_list", setting the layoutManager and adapter for it
          * and return a reference to the View that is the root of the inflated layout.
          */
-        val view = inflater.inflate(R.layout.monster_item_list, container, false)
-        (view.rvMonsterList as RecyclerView).apply {
+        val mlView = inflater.inflate(R.layout.monster_item_list, container, false)
+        (mlView?.rvMonsterList as RecyclerView).apply {
             layoutManager = LinearLayoutManager(context)
             adapter = MonsterAdapter(mutableListOf())
         }
-        val fetchBtn = view.findViewById<Button>(R.id.fetchBtn)
-        val clearBtn = view.findViewById<Button>(R.id.clearBtn)
-        fetchBtn?.setOnClickListener(this)
-        clearBtn?.setOnClickListener(this)
-
-        return view
-    }
-    override fun onClick(view: View) {
-        /* Handle click event based on id of view (button). For fetch button, pass a callback to the
-         * constructor of UpdateMonsterList to retrieve a List<Monster> For clear button, .clear()
-         * is just being called on the adapter's List<Monster>.
-         * In either case, notifyDataSetChanged() is called on the adapter after modifying the list
-         * to let it know it should update its UI.
-         */
-        when(view.id) {
-            R.id.fetchBtn -> {
-                this.view?.loadingSpinner?.visibility = View.VISIBLE
-                UpdateMonsterList { ml ->
-                    (this.view?.rvMonsterList?.adapter as? MonsterAdapter)?.monsters =
-                            ml.toMutableList()
-                    this.view?.rvMonsterList?.adapter?.notifyDataSetChanged()
-                    this.view?.loadingSpinner?.visibility = View.INVISIBLE
-                }.execute()
-            }
-            R.id.clearBtn -> {
-                (this.view?.rvMonsterList?.adapter as? MonsterAdapter)?.monsters?.clear()
-                this.view?.rvMonsterList?.adapter?.notifyDataSetChanged()
-            }
+        mlView.fetchBtn?.setOnClickListener{
+            mlView.loadingSpinner?.visibility = View.VISIBLE
+            UpdateMonsterList { ml ->
+                (mlView.rvMonsterList?.adapter as? MonsterAdapter)?.monsters =
+                        ml.toMutableList()
+                mlView.rvMonsterList?.adapter?.notifyDataSetChanged()
+                mlView.loadingSpinner?.visibility = View.INVISIBLE
+            }.execute()
         }
+        mlView.clearBtn?.setOnClickListener{
+            (mlView.rvMonsterList?.adapter as? MonsterAdapter)?.monsters?.clear()
+            mlView.rvMonsterList?.adapter?.notifyDataSetChanged()
+        }
+        return mlView
     }
 }
 
