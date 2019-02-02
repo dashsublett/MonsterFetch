5074185f19874ceb540f96ab7f880501595aa635
Dashiell Sublett <dsublett@inmotionsoftware.com>
2019-02-01 18:47:29 -0600

    greatly cleaned up ItemFragment code

--- app/src/main/java/com/example/dsublett/monsterfetch/ItemFragment.kt
+++ app/src/main/java/com/example/dsublett/monsterfetch/ItemFragment.kt
@@ -4,7 +4,6 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,36 +15,27 @@ class ItemFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        /* Inflate RecyclerView "monster_item_list", setting the layoutManager and adapter for it
-         * and return a reference to the View that is the root of the inflated layout.
-         */
-        val mlView = inflater.inflate(R.layout.monster_item_list, container, false)
-        (mlView?.rvMonsterList as RecyclerView).apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = MonsterAdapter(mutableListOf())
+        return inflater.inflate(R.layout.monster_item_list, container, false).apply {
+            this.fetchBtn.setOnClickListener{
+                this.loadingSpinner.visibility = View.VISIBLE
+                UpdateMonsterList { ml ->
+                    (this.rvMonsterList.adapter as MonsterAdapter).monsters = ml.toMutableList()
+                    this.rvMonsterList.adapter?.notifyDataSetChanged()
+                    this.loadingSpinner.visibility = View.INVISIBLE
+                }.execute()
+            }
+            this.clearBtn.setOnClickListener{
+                (this.rvMonsterList.adapter as MonsterAdapter).monsters.clear()
+                this.rvMonsterList.adapter?.notifyDataSetChanged()
+            }
+            this.rvMonsterList.layoutManager = LinearLayoutManager(context)
+            this.rvMonsterList.adapter = MonsterAdapter(mutableListOf())
         }
-        mlView.fetchBtn?.setOnClickListener{
-            mlView.loadingSpinner?.visibility = View.VISIBLE
-            UpdateMonsterList { ml ->
-                (mlView.rvMonsterList?.adapter as? MonsterAdapter)?.monsters =
-                        ml.toMutableList()
-                mlView.rvMonsterList?.adapter?.notifyDataSetChanged()
-                mlView.loadingSpinner?.visibility = View.INVISIBLE
-            }.execute()
-        }
-        mlView.clearBtn?.setOnClickListener{
-            (mlView.rvMonsterList?.adapter as? MonsterAdapter)?.monsters?.clear()
-            mlView.rvMonsterList?.adapter?.notifyDataSetChanged()
-        }
-        return mlView
     }
 }
 
 private class UpdateMonsterList(private val callback: ((List<Monster>) -> Unit))
     : AsyncTask<Unit, Unit, List<Monster>>() {
-    /* Performs GET request, parses the response and returns that list or empty list depending on
-     * success of the operation.
-     */
     override fun doInBackground(vararg p0: Unit): List<Monster>? {
         val monsterList = Moshi
                 .Builder()
