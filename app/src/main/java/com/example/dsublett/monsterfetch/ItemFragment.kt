12b5386db856ec03a68a1dd51b426e72f7878ccb
Dashiell Sublett <dsublett@inmotionsoftware.com>
2019-02-02 12:24:10 -0600

    created services package and moved UpdateMonsterList there

--- app/src/main/java/com/example/dsublett/monsterfetch/ItemFragment.kt
+++ app/src/main/java/com/example/dsublett/monsterfetch/ItemFragment.kt
@@ -1,15 +1,14 @@
 package com.example.dsublett.monsterfetch
 
-import android.os.AsyncTask
+import com.example.dsublett.monsterfetch.services.UpdateMonsterList
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.squareup.moshi.Moshi
 import kotlinx.android.synthetic.main.monster_item_list.view.*
-import java.net.URL
 
 class ItemFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
@@ -31,22 +30,5 @@ class ItemFragment : Fragment() {
             this.rvMonsterList.layoutManager = LinearLayoutManager(context)
             this.rvMonsterList.adapter = MonsterAdapter(mutableListOf())
         }
-    }
-}
-
-private class UpdateMonsterList(private val callback: ((List<Monster>) -> Unit))
-    : AsyncTask<Unit, Unit, List<Monster>>() {
-    override fun doInBackground(vararg p0: Unit): List<Monster>? {
-        val monsterList = Moshi
-                .Builder()
-                .build()
-                .adapter(Response::class.java)
-                .fromJson(URL("http://www.dnd5eapi.co/api/monsters").readText())
-                ?.results
-        return monsterList ?: emptyList()
-    }
-    override fun onPostExecute(result: List<Monster>) {
-        super.onPostExecute(result)
-        this.callback(result)
     }
 }
