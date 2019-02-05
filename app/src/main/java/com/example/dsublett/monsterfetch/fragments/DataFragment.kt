592665e476a3904c70c80f185a8c172a5b5b0a57
Dashiell Sublett <dsublett@inmotionsoftware.com>
2019-02-05 10:16:28 -0600

    moved onCreateView and data fetch callback logic into abstract DataFragment class

---
+++ app/src/main/java/com/example/dsublett/monsterfetch/fragments/DataFragment.kt
@@ -0,0 +1,42 @@
+package com.example.dsublett.monsterfetch.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dsublett.monsterfetch.R
+import com.example.dsublett.monsterfetch.adapters.ItemAdapter
+import com.example.dsublett.monsterfetch.models.DNDAPIResponse
+import kotlinx.android.synthetic.main.item_list.*
+import kotlinx.android.synthetic.main.item_list.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+abstract class DataFragment : Fragment() {
+    abstract fun fetchData()
+    protected val fetchCallback = object: Callback<DNDAPIResponse> {
+        override fun onResponse(call: Call<DNDAPIResponse>, response: Response<DNDAPIResponse>) {
+            (rvItemList.adapter as ItemAdapter).responseItems =
+                    response.body()?.results!!.toMutableList()
+            rvItemList.adapter?.notifyDataSetChanged()
+            loadingSpinner.visibility = View.INVISIBLE
+        }
+        override fun onFailure(call: Call<DNDAPIResponse>, t: Throwable) {
+            loadingSpinner.visibility = View.INVISIBLE
+        }
+    }
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val returnView = inflater.inflate(R.layout.item_list, container, false).apply {
+            this.rvItemList.layoutManager = LinearLayoutManager(context)
+            this.rvItemList.adapter = ItemAdapter(mutableListOf())
+            this.loadingSpinner.visibility = View.VISIBLE
+        }
+        fetchData()
+        return returnView
+    }
+}
--- app/src/main/java/com/example/dsublett/monsterfetch/fragments/MonstersFragment.kt
+++ app/src/main/java/com/example/dsublett/monsterfetch/fragments/MonstersFragment.kt
@@ -1,44 +1,9 @@
 package com.example.dsublett.monsterfetch.fragments
 
-import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.dsublett.monsterfetch.adapters.ItemAdapter
-import com.example.dsublett.monsterfetch.R
-import com.example.dsublett.monsterfetch.models.DNDAPIResponse
 import com.example.dsublett.monsterfetch.services.DNDAPIService
-import kotlinx.android.synthetic.main.item_list.view.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-class MonstersFragment : Fragment() {
-    private val TAG = this::class.java.simpleName
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.item_list, container, false).apply {
-            this.rvItemList.layoutManager = LinearLayoutManager(context)
-            this.rvItemList.adapter = ItemAdapter(mutableListOf())
-            this.loadingSpinner.visibility = View.VISIBLE
-
-            DNDAPIService.create().getMonsters().enqueue(object: Callback<DNDAPIResponse> {
-                override fun onResponse(call: Call<DNDAPIResponse>,
-                                        response: Response<DNDAPIResponse>) {
-                    (rvItemList.adapter as ItemAdapter).responseItems =
-                            response.body()?.results!!.toMutableList()
-                    rvItemList.adapter?.notifyDataSetChanged()
-                    loadingSpinner.visibility = View.INVISIBLE
-                }
-                override fun onFailure(call: Call<DNDAPIResponse>, t: Throwable) {
-                    Log.d(TAG, "getMonsters() call failed: $t")
-                    loadingSpinner.visibility = View.INVISIBLE
-                }
-            })
-        }
+class MonstersFragment : DataFragment() {
+    override fun fetchData() {
+        DNDAPIService.create().getMonsters().enqueue(fetchCallback)
     }
 }
