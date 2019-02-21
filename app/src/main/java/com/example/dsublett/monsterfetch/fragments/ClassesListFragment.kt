ca4279a66727c831141f523563c145d108684c70
Dashiell Sublett <dsublett@inmotionsoftware.com>
2019-02-21 17:50:41 -0600

    defined buildUI and logFailure methods for ItemListFragment that implementing classes pass to dndService calls

--- app/src/main/java/com/example/dsublett/monsterfetch/fragments/ClassesListFragment.kt
+++ app/src/main/java/com/example/dsublett/monsterfetch/fragments/ClassesListFragment.kt
@@ -1,28 +1,7 @@
 package com.example.dsublett.monsterfetch.fragments
 
-import android.util.Log
-import android.view.View
-import com.example.dsublett.monsterfetch.adapters.ItemAdapter
-import com.example.dsublett.monsterfetch.models.ResponseItem
 import com.example.dsublett.monsterfetch.services.ServiceProxy
-import kotlinx.android.synthetic.main.item_list.*
 
 class ClassesListFragment : ItemListFragment() {
-    override fun fetchData() = ServiceProxy.dndService.getClasses(
-        {
-            this.rvItemList?.adapter =
-                ItemAdapter(it,
-                    object : ItemAdapter.OnItemClickListener {
-                        override fun onItemClick(responseItem: ResponseItem) {
-                            this@ClassesListFragment.showable?.showDetails(responseItem)
-                        }
-                    }
-                )
-            this.loadingSpinner?.visibility = View.INVISIBLE
-        },
-        {
-            this.loadingSpinner?.visibility = View.INVISIBLE
-            Log.d(this@ClassesListFragment::class.java.canonicalName, "$it")
-        }
-    )
+    override fun fetchData() = ServiceProxy.dndService.getClasses(this::buildUI, this::logFailure)
 }
